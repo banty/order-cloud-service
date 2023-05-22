e7db30c0518cacd38bc1dfc485e4ca5af165d1d5
Banty Bahru <bantyder@gmail.com>
2023-05-22 11:13:45 +0200

    add domain custom exception

---
+++ EasyOrder.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace EasyOrder.Domain.Exceptions
+{
+	public class DomainException:Exception
+	{
+		public DomainException():base("Domain exception occured")
+		{
+		}
+		public DomainException(string message):base(message)
+		{
+
+		}
+
+		public DomainException(string message, Exception? ex) :base(message,ex)
+		{
+
+		}
+	}
+}
+
